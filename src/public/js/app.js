72d8db63b46a8069338d9dfddd25eed7de4443fe
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2022-08-11 13:59:06 +0900

    .

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,41 @@
 const socket = io();
 
+const myFace = document.querySelector("#myFace");
+const selectCam = document.querySelector("#select-cam");
 const nickForm = document.querySelector("#nickForm");
 const roomForm = document.querySelector("#roomForm");
 const msgForm = document.querySelector("#msgForm");
+
+const muteBtn = myFace.querySelector("#mute");
+const cameraBtn = myFace.querySelector("#camera");
+
+let myStream;
+let isMute = false;
+let isCameraOff = false;
+
+async function getMedia(){
+    const video = myFace.querySelector("video");
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:true,
+        });
+        video.srcObject = myStream;
+    }catch(e){
+        console.log(e);
+    }
+}
+
+async function getCam(){
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cam = devices.filter((device)=> device.kind === "videoinput");
+    cam.forEach((camera) => {
+        const option = document.createElement("option");
+        option.value = camera.deviceId;
+        option.innerText = camera.label;
+        selectCam.appendChild(option);
+    });
+}
 
 function addMsg(message){
     const ul = document.querySelector("#msgDiv ul");
@@ -47,11 +80,45 @@ function handleToMsgSubmit(event){
     input.value = "";
 }
 
+function handleToMuteClick(event){
+    event.preventDefault();
+    myStream.getAudioTracks().forEach((track) => {
+        track.enabled = !track.enabled;
+    });
+    if(!isMute){
+        isMute = true;
+        muteBtn.innerText = "Unmute";
+    }else{
+        isMute = false;
+        muteBtn.innerText = "Mute";
+    }
+}
+
+function handleToCameraClick(event){
+    event.preventDefault();
+    myStream.getVideoTracks().forEach((track) => {
+        track.enabled = !track.enabled;
+    });
+    if(!isCameraOff){
+        isCameraOff = true;
+        cameraBtn.innerText = "Turn on";
+    }else{
+        isCameraOff = false;
+        cameraBtn.innerText = "Turn off";
+    }
+
+}
 msgForm.hidden = true;
+getMedia();
+getCam();
+muteBtn.addEventListener("click", handleToMuteClick);
+cameraBtn.addEventListener("click", handleToCameraClick);
 
 nickForm.addEventListener("submit", handleToNickSubmit)
 roomForm.addEventListener("submit", handleToRoomSubmit);
 msgForm.addEventListener("submit", handleToMsgSubmit);
+
+
 
 socket.on("msg", (msg, nickname)=>{
     addMsg(`${nickname}: ${msg}`);
